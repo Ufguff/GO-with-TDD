3531ad0985de17bc34a288e9db771f58138f9dfd
Ufguff <tima.shchipkov@mail.ru>
2024-08-08 16:10:51 +0500

    done with 6, errcheck not working :(

---
+++ 6_pointers/poiners_test.go
@@ -0,0 +1,46 @@
+package pointers
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+
+	})
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+	t.Run("withdraw with - amount", func(t *testing.T) {
+		startBalance := Bitcoin(20)
+		wallet := Wallet{startBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, "cannot withdraw, insufficient funds")
+		assertBalance(t, wallet, startBalance)
+	})
+
+}
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+func assertError(t testing.TB, got error, want string) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("wanted an error but didn't get one")
+	}
+
+	if got.Error() != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
